c2e714467f31f7fa2e2b76d3cf518c835510d132
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-06-05 08:27:01 +0300

    detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+        for w in word_list:
+            sorted_w = sorted(w.lower())
+            if sorted_word == sorted_w and self.word != w.lower():
+                matches.append(w)
+
+        return matches
